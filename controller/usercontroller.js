c1b54cc6d2e85992fc5c4b757e30d65a77348bb6
Dishant <dishantpatel1446@gmail.com>
2024-01-25 16:56:53 +0530

    front change

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -16,31 +16,10 @@ module.exports.home = async(req,res)=>{
         productData : product
     });
 }
-module.exports.cateProduct = async(req,res)=>{
-    let catData = await Product.find({category : req.params.id}).populate('brandname').exec();
-    let Category = await Cate.find({isActive : true});
-    let Subcategory = await Scate.find({isActive : true});
-    let Extracategory = await Ecate.find({isActive : true});
-    let recentPost = await Product.find({}).sort({ id: -1 }).limit(3);
-    var brands =[];
-    catData.forEach(v => {
-        let pos = brands.findIndex((v1,i1)=> v1.id==v.brandname.id);
-        if(pos==-1){
-            brands.push({id : v.brandname.id , name : v.brandname.brandname});
-        }
-    });
-    return res.render('UserPanel/product',{
-        cate : Category,
-        subcate : Subcategory,
-        ecate : Extracategory,
-        productData : catData,
-        brand : brands,
-        recentPost : recentPost
-    })
-}
 module.exports.filterBrand = async(req,res)=>{
     let catData = await Product.find({category : req.body.cid,subcategory:req.body.sid,extracategory:req.body.eid,brandname:req.body.id});
     let product = await Product.find({category : req.body.cid});
+    catData
     return res.render('UserPanel/filter',{
         productData : catData,
         product : product
@@ -59,6 +38,19 @@ module.exports.findData = async(req,res)=>{
             brands.push({id : v.brandname.id , name : v.brandname.brandname});
         }
     });
+    var max = 0;
+    product.map((v,i)=>{
+        if(parseInt(v.price)>max){
+            max = parseInt(v.price);
+        }
+    })
+    min = max;
+
+    product.map((v,i)=>{
+        if(parseInt(v.price)<min){
+            min = parseInt(v.price);
+        }
+    })
     return res.render('UserPanel/product',{
         productData : product,
         cate : Category,
@@ -68,6 +60,20 @@ module.exports.findData = async(req,res)=>{
         recentPost : recentPost,
         cid : req.params.cid,
         sid : req.params.sid,
-        eid : req.params.eid
+        eid : req.params.eid,
+        min:min,
+        max:max,
     });
+}
+module.exports.gotocart = async(req,res)=>{
+    let singleproduct = await Product.findById(req.params.id);
+    let Category = await Cate.find({isActive : true});
+    let Subcategory = await Scate.find({isActive : true});
+    let Extracategory = await Ecate.find({isActive : true});
+    return res.render('UserPanel/singleproduct',{
+        cate : Category,
+        subcate : Subcategory,
+        ecate : Extracategory,
+        details : singleproduct
+    })
 }
--- routs/user.js
+++ routs/user.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routs = express.Router();
 const usercontroller = require('../controller/usercontroller');
 routs.get('/',usercontroller.home);
-routs.get('/catproduct/:id',usercontroller.cateProduct);
 routs.post('/filterBrand',usercontroller.filterBrand);
 routs.get('/product/:cid/:sid/:eid',usercontroller.findData);
+routs.get('/gotocart/:id',usercontroller.gotocart);
 module.exports = routs;
